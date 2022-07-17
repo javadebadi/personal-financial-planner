3d12858125f86012d4205214496d1c2b9c38fdf9
javadebadi <javad.ebadi.1990@gmail.com>
2022-07-17 19:38:06 +0430

    Added a simple dashboard view for pfp app

--- pfp/urls.py
+++ pfp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from pfp.views import (
+    DashboardView,
+)
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    
+    path('', login_required(DashboardView.as_view()), name='pfp_dasboard'),
 ]
--- pfp/views.py
+++ pfp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
-# Create your views here.
+class DashboardView(PermissionRequiredMixin, View):
+    permission_required =  (
+        'subscribed',
+        )
+
+    def get(self, request):
+        return HttpResponse("<h1>Hello Personal Financial Planner</h1>")
