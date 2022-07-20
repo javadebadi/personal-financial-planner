5010a803d25466d5c0b03abcf1973999799759d3
javadebadi <javad.ebadi.1990@gmail.com>
2022-07-20 18:36:05 +0430

    commited forgotten file

--- pfp/views.py
+++ pfp/views.py
@@ -11,10 +11,11 @@ class DashboardView(PermissionRequiredMixin, View):
 
     def get(self, request):
         user = request.user
-        br = BankAccountBR()
+        br = BankAccountBR(user)
         context = {
             'title': 'dashboard',
-            'bank_accounts': br.display(user),
+            'bank_accounts': br.display(),
+            'total_balance': br.display_total_balance(),
         }
         print(context)
         return render(request, 'pfp/base.html', context=context)
