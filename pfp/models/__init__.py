8e7240acceaf261661e40ed429ad60cbad7032e0
javadebadi <javad.ebadi.1990@gmail.com>
2022-07-21 13:48:19 +0430

    Added model for transaction

--- pfp/models/__init__.py
+++ pfp/models/__init__.py
@@ -1,4 +1,8 @@
+import imp
 from .bank_accounts import (
     Bank,
     BankAccount,
+)
+from .transaction import (
+    Transaction,
 )
---
+++ pfp/models/transaction.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.timezone import make_naive
+from pfp.models import BankAccount
+
+User = get_user_model()
+
+
+class Transaction(models.Model):
+
+    transaction_id = models.CharField(
+        max_length=48,
+        primary_key=True,
+    )
+
+    bank_account = models.ForeignKey(
+        BankAccount,
+        on_delete=models.CASCADE,
+        db_column='bank_account_id',
+        related_name='transactions',
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        db_column='user_id',
+        related_name='transactions',
+    )
+
+    amount = models.DecimalField(
+        max_digits=18,
+        decimal_places=2,
+        default=0,
+        )
+
+    transaction_time = models.TimeField()
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+    def save(self, *args, **kwargs) -> str:
+        if self.transaction_id is None:
+            return super(Transaction, self).save(*args, **kwargs)
+        else:
+            self.transaction_id = str(self.user.id).rjust(10, '0') + '-'
+            self.transaction_id += str(make_naive(self.transaction_time)) + '-'
+            self.transaction_id +=  str(self.bank_account.bank_account_id).rjust(10, '0')
+            return super(Transaction, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = (
+            '-transaction_time',
+        )
+
