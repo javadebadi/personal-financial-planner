552e9ae58176f035e8aa1b5703852de2375ffb92
javadebadi <javad.ebadi.1990@gmail.com>
2022-07-26 19:24:14 +0430

    updated admin panel for pfp

--- pfp/admin.py
+++ pfp/admin.py
@@ -3,6 +3,8 @@ from pfp.models import (
     Bank,
     BankAccount,
     Transaction,
+    AssetCategory,
+    AssetSubCategory,
 )
 
 # Register your models here.
@@ -18,6 +20,33 @@ class TranactionAdmin(admin.ModelAdmin):
         'amount',
         'bank_account',
         'inflow',
+        )
+
+    list_filter = (
+        'user',
+    )
+
+
+@admin.register(AssetSubCategory)
+class AssetSubCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'asset_subcategory_id',
+        'user',
+        'name',
+        'category',
+        )
+
+    list_filter = (
+        'user',
+    )
+
+
+@admin.register(AssetCategory)
+class AssetCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'asset_category_id',
+        'user',
+        'name',
         )
 
     list_filter = (
