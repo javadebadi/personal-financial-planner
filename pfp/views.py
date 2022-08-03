52954d33792936e5b8f77f392528920a108b5044
javadebadi <javad.ebadi.1990@gmail.com>
2022-08-03 18:20:04 +0430

    Added a view to get list of asset categories which are specific to a user

--- pfp/views.py
+++ pfp/views.py
@@ -1,8 +1,25 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from pfp.business_rules import BankAccountBR
+from pfp.business_rules.asset import AssetBR
+
+class AssetCategoyListView(PermissionRequiredMixin, View):
+    permission_required =  (
+        'subscribed',
+        )
+    
+    def get(self, request):
+        user = request.user
+        br = AssetBR(user=user)
+        context = {
+            "title": "assets",
+            "assets_categories": br.get_categories(),
+        }
+        return render(request, 'pfp/assets.html', context=context)
+
 
 class DashboardView(PermissionRequiredMixin, View):
     permission_required =  (
