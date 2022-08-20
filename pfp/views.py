f0284ba1926bb61391601bc6ceb1d36fd061b9ce
javadebadi <javad.ebadi.1990@gmail.com>
2022-08-20 14:16:26 +0430

    Added a view for asset category view

--- pfp/views.py
+++ pfp/views.py
@@ -2,11 +2,12 @@ from multiprocessing import context
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from pfp.business_rules import BankAccountBR
 from pfp.business_rules.asset import AssetBR
 
-class AssetCategoyListView(PermissionRequiredMixin, View):
+
+class AssetCategoyListView(LoginRequiredMixin, PermissionRequiredMixin, View):
     permission_required =  (
         'subscribed',
         )
@@ -18,10 +19,33 @@ class AssetCategoyListView(PermissionRequiredMixin, View):
             "title": "assets",
             "assets_categories": br.get_categories(),
         }
-        return render(request, 'pfp/assets.html', context=context)
+        return render(
+            request,
+            'pfp/assets_categories.html',
+            context=context,
+            )
 
 
-class DashboardView(PermissionRequiredMixin, View):
+class AssetsListView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required =  (
+        'subscribed',
+        )
+    
+    def get(self, request):
+        user = request.user
+        br = AssetBR(user=user)
+        context = {
+            "title": "assets",
+            "assets_categories": br.get_categories(),
+        }
+        return render(
+            request,
+            'pfp/assets.html',
+            context=context,
+            )
+
+
+class DashboardView(LoginRequiredMixin, PermissionRequiredMixin, View):
     permission_required =  (
         'subscribed',
         )
@@ -34,5 +58,8 @@ class DashboardView(PermissionRequiredMixin, View):
             'bank_accounts': br.display(),
             'total_balance': br.display_total_balance(),
         }
-        print(context)
-        return render(request, 'pfp/base.html', context=context)
+        return render(
+            request,
+            'pfp/base.html',
+            context=context,
+            )
